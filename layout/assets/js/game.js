2a57acba11fa3311d041a589dd072416123cd0a8
TODAISY <91897155+todaisy@users.noreply.github.com>
2023-03-24 12:19:24 +0300

    Update game.js

--- layout/assets/js/game.js
+++ layout/assets/js/game.js
@@ -1,9 +1,35 @@
 class Game{
     constructor() {
         this.name = name;
+        this.$zone = $('.elements')
+        this.elements = [];
     }
     start() {
         this.loop();
+    }
+
+    loop() {
+        requestAnimationFrame(() => {
+            this.updateElements();
+            this.setParams();
+            this.loop();
+        })
+    }
+    
+    setParams() {
+        let params = ['name'];
+        let value = [this.name];
+    
+        params.forEach((e, i) => {
+            $(`#${e}`).html(value[i]);
+        })
+    }
+
+    updateElements(){
+        this.elements.forEach(e => {
+            e.update();
+            e.draw();
+        })
     }
 
 }
@@ -21,3 +47,37 @@ window.onload = () => {
     }, 500)
 }
 
+
+class Drawable {
+    constructor(game) {
+        this.game = game;
+        this.x = 0;
+        this.y = 0;
+        this.h = 0;
+        this.w = 0;
+        this.offsets = {
+            x: 0, 
+            y: 0
+        }
+    }
+
+    createElement() {
+        this.$element = $(`<div class="element ${this.constructor.name.toLocaleLowerCase()}"></div>`);
+        this.game.$zone.append(this.$element);
+    }
+
+    update() {
+        this.x += this.offsets.x;
+        this.y += this.offsets.y;
+    }
+
+    draw() {
+        this.$element.css({
+            left: this.x + "px",
+            top: this.y + "px",
+            width: this.w + "px",
+            height: this.h + "px"
+        })
+    }
+
+}
